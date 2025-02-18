9ca2f9644365df4aed89b5140c6d9aba895b7190
D <dector@dector.space>
2025-02-18 19:36:12 +0100

    add (fish): more aliases

--- private_dot_config/private_fish/conf.d/__aliases.fish
+++ private_dot_config/private_fish/conf.d/__aliases.fish
@@ -4,10 +4,18 @@ alias rm-hard /usr/bin/rm
 
 # Development
 alias r task
+alias lg lazygit
 alias g git
+alias gs 'git status'
 alias j jj
 alias jl 'jj log --reversed'
 
 # Clipboard
 alias clp 'xclip -sel c'
+
+# Always ignore case for grep
+alias grep 'grep -i'
+
+# Printing
+alias log 'echo -n'
 
