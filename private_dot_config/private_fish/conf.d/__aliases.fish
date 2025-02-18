2bef3da824c43e5549b42c9ce730c641f109a6f8
D <dector@dector.space>
2025-02-18 19:28:24 +0100

    new (fish): add aliases

---
+++ private_dot_config/private_fish/conf.d/__aliases.fish
@@ -0,0 +1,13 @@
+# Remove to trash
+alias rm trash
+alias rm-hard /usr/bin/rm
+
+# Development
+alias r task
+alias g git
+alias j jj
+alias jl 'jj log --reversed'
+
+# Clipboard
+alias clp 'xclip -sel c'
+
