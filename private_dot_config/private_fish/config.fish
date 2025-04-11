f4d2bd0c5268714a431eeb5ec04539fc7104635b
D <dector@dector.space>
2025-04-11 17:57:06 +0200

    (fish): add brew
    also activate shell extensions non-interactively
    as it seems to fail sometimes in app-embedded terminals

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,13 @@
+fish_add_path ~/.local/bin
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+mise activate | source
+starship init fish | source
+atuin init --disable-up-arrow fish | source
+
 if status is-interactive
-  starship init fish | source
-  atuin init --disable-up-arrow fish | source
+  #starship init fish | source
+  #atuin init --disable-up-arrow fish | source
 end
 
 if test -e priv.config.fish
